e42295a5c0efb7a393bcb470997664e4259403bc
Moataz Hamdan <hamdanmoataz@gmail.com>
2023-07-27 11:20:34 -0500

    fixed conditional statement!

--- homework.py
+++ homework.py
@@ -1,15 +1,20 @@
+
 import requests
+# import Image
+
 
 class Pokemon:
-    def __init__(self, name, height, weight):
+    def __init__(self, name, height, weight ):# image_url
         self.name=name
         self.height=height
         self.weight=weight
+       # self.image_url=image_url
 
     def __str__(self):
         output = f"----{self.name.title()}----\n"
         output += f"Height: {self.height}\n"
         output += f"Weight: {self.weight} \n"
+        #output += f"Image: {self.image_url} \n"
         return output
     
     def __repr__(self):
@@ -26,18 +31,20 @@ class PokemonAPI:
         res = requests.get(f"{self.base_url}{poke_name}")
         if res.ok:
             return res.json()
-        return f"{poke_name} isn't a pokemon!"
+        return 
 
 
     def get_pokemon(self, pokemon_name):
         data = self.__get(pokemon_name)
-        if pokemon_name in data:
+        if not data:
             return f'{pokemon_name.title()} isnt a pokemon!'
         else:
             name = data['name']
             height = data['height']
             weight = data['weight']
-            new_pokemon = Pokemon(name,height,weight)
+            # image_url=(Image(f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/{pokemon_name}.png"))
+            
+            new_pokemon = Pokemon(name,height,weight)#,image_url)
             return new_pokemon
     
     
